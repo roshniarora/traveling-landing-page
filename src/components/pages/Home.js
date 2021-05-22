800d17a6239df11582fafca076fc9b9c7b709c2a
roshniarora <roshnibly95@gmail.com>
2021-05-22 22:52:36 +0530

    first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import "../App.css";
+
+import Section from "../Section";
+
+function Home() {
+  return (
+    <div>
+      <Section />
+    </div>
+  );
+}
+
+export default Home;
